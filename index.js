4952e69eb3f1aa77906e9b57ea0287f16d1edb94
Maliheim <reeveslance@gmail.com>
2022-10-05 20:13:50 -0700

    changed onsubmit from button to form || still unable to get event.preventDefault to work

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 let form = document.getElementById("inputform");
 let inputbox = document.getElementById("inputbox");
 let button = document.getElementById("submitbtn");
-
 let eformat = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
 
 let errorTracker = false;
 
-let contactList = []
+let contactList = [];
 errorTracker = false;
 
-let addContact = (email) => {contactList.push(email)};
+let addContact = (email) => {
+  contactList.push(email);
+};
 
-let submit = (e) => {
-    e.preventDefault()
-    console.log(inputbox.value)
+let submit = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  console.log(inputbox.value);
 
-if(eformat.test(inputbox.value)){
-    addContact(inputbox.value)
-    input.value = ""
-    error.style.display = "none"
+  if (eformat.test(inputbox.value)) {
+    addContact(inputbox.value);
+    input.value = "";
+    let error = document.getElementById("err");
+    error.style.display = "none";
     errorTracker = false;
-    console.log(contactList)
-}else{
-    let err = document.createElement("p")
-    err.id = "err"
-    err.innerHTML = "Please provide a valid email"
-    input.insertAdjacentElement
-}}
-
-console.log(inputbox.value)
+    console.log(contactList);
+  } else {
+    if (!errorTracker) {
+      let err = document.createElement("p");
+      err.id = "err";
+      err.innerHTML = "Please provide a valid email";
+      input.insertAdjacentElement;
+    }
+  }
+};
+console.log(contactList);
